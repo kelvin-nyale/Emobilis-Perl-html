a435f77389b5f9b0b56ca45149aa8453a7bba465
kelvin-nyale <kelvinnyale780@gmail.com>
2024-10-16 13:06:50 +0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+
+
+
+const colorButton= document.getElementById('colorButton');
+
+colorButton.addEventListener('click', ()=>{
+    colorButton.style.backgroundColor=colorButton.style.backgroundColor === '#ff0'? '#00f':'#ff0';
+});
